be4592e0ee68354cd7296aa2a70bf3fd705995a4
Maxime <>
2025-08-11 23:51:22 +0200

    feat: base code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::error::Error;
+
+struct VulkanApp;
+
+impl VulkanApp {
+    fn new() -> Result<Self, Box<dyn Error>> {
+        Ok(Self)
+    }
+    fn run(&mut self) {
+        println!("Running application")
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    match VulkanApp::new() {
+        Ok(mut app) => app.run(),
+        Err(err) => eprintln!("Failed to create application: {err}"),
+    }
 }
