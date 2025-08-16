bad665f7bb32d5d2d2e880c763cf0277dc0bffde
Maxime <>
2025-08-16 15:01:06 +0200

    feat: create the vulkan instance

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ use vulkan::VulkanApp;
 mod vulkan;
 
 fn main() {
-    let mut vulkan_app = VulkanApp::new();
-    match vulkan_app.run() {
-        Err(err) => println!("Err:{err}"),
-        _ => println!("scop closed correctely"),
+    if let Ok(mut vulkan_app) = VulkanApp::new() {
+        match vulkan_app.run() {
+            Err(err) => println!("Err:{err}"),
+            _ => println!("scop closed correctely"),
+        }
     }
 }
--- src/vulkan.rs
+++ src/vulkan.rs
@@ -1,17 +1,42 @@
+use ash::vk::{make_api_version,InstanceCreateInfo, ApplicationInfo, API_VERSION_1_3};
+use ash::{Entry,Instance};
+
+
 use std::error::Error;
+
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::Window;
 
 mod window;
 
-#[derive(Default)]
 pub struct VulkanApp {
     window: Option<Window>,
+    entry: Entry,
+    instance: Instance,
 }
 
 impl VulkanApp {
-    pub fn new() -> Self {
-        VulkanApp::default()
+    pub fn new() -> Result<Self, Box<dyn Error>> {
+        let entry = unsafe { Entry::load()? };
+        let instance = Self::create_instance(&entry)?;
+        Ok(Self {
+            window: None,
+            entry,
+            instance,
+        })
+    }
+
+    fn create_instance(entry: &Entry) -> Result<Instance, Box<dyn Error>> {
+        let app_info: ApplicationInfo = ApplicationInfo::default();
+        app_info.application_name(c"scop");
+        app_info.application_version(make_api_version(0, 1, 0, 0));
+        app_info.engine_name(c"scop_engine");
+        app_info.engine_version(make_api_version(0, 1, 0, 0));
+        app_info.api_version(API_VERSION_1_3);
+        
+        let instance_create_info = InstanceCreateInfo::default().application_info(&app_info);
+
+        unsafe { Ok(entry.create_instance(&instance_create_info, None)?) }
     }
 
     pub fn run(&mut self) -> Result<(), Box<dyn Error>> {
